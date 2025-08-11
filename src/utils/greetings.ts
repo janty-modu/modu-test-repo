88369854a641a9f0f587e33c308981b83de58226
Modu <modu@ohara.ai>
2025-08-11 14:48:18 +0000

    Add greeting functions: hello world, goodbye world, good morning, and good night

---
+++ src/utils/greetings.ts
@@ -0,0 +1,19 @@
+/**
+ * Greeting utility functions
+ */
+
+export function helloWorld(): string {
+  return "Hello, World!";
+}
+
+export function goodByeWorld(): string {
+  return "Goodbye, World!";
+}
+
+export function goodMorning(): string {
+  return "Good Morning!";
+}
+
+export function goodNight(): string {
+  return "Good Night!";
+}
